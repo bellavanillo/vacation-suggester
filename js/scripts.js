38a2f866f39f552caeb7d2399a9c36c219ac4bb2
bv <bellavanillo@gmail.com>
2018-03-09 15:26:05 -0800

    fix javascript bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
-  $("form.survey").submit(function(event){
+  $("form#survey").submit(function(event){
     event.preventDefault();
     var weather = $("input:radio[name=weather]:checked").val();
     var environment = $("input:radio[name=environment]:checked").val();
     var food = $("input:radio[name=food]:checked").val();
-    $("#result").show();
-if ((weather === "hot") && (environment === "bigcity") && (food === "rio")){
-  $(".rioresult").show();
-  $(".icelandresult").hide();
-  $(".sbresult").hide();
-   } else if ((weather === "cold") && (environment === "rural") && (food === "ice")){
-     $(".icelandresult").show();
-     $("rioresult").hide();
-     $(".sbresult").hide();
-   } else {
-     $(".sbresult").show();
-     $("rioresult").hide();
-     $(".icelandresult").hide();
-   }
+
+    $(".sbresult").hide();
+    $(".rioresult").hide();
+    $(".icelandresult").hide();
+
+  if (weather === "hot" && environment === "bigcity" && food === "rio") {
+    $(".sbresult").show();
+    $(".rioresult").hide();
+    $(".icelandresult").hide();
+  } else if (weather === "cold" && environment === "rural" && food === "ice") {
+    $(".sbresult").hide();
+    $(".rioresult").hide();
+    $(".icelandresult").show();
+  } else if (weather === "mild" && environment === "beach" && food === "sb") {
+    $(".sbresult").show();
+    $(".rioresult").hide();
+    $(".icelandresult").hide();
+  } else {
+    $("#result").hide();
+  }
+
+  $("#survey").trigger("reset");
+
  });
 });
